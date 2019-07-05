578375410eb4151d8a574581f99c6fe077e7d216
Aleksandar Stojanoski <aleksandar-stojanoski@hotmail.com>
2019-07-05 21:25:18 +0200

    update set-value mutation

--- src/mutations/set-value.js
+++ src/mutations/set-value.js
@@ -2,8 +2,13 @@ import { set } from 'lodash';
 import { isString } from '../utils/is-string';
 
 export const SET_VALUE = (state, { stateKey, value }) => {
-    if (!stateKey || !isString(stateKey)) {
+    if (!stateKey) {
         return;
     }
+
+    if (!isString(stateKey)) {
+        return;
+    }
+
     set(state, stateKey, value);
 };
